eff19a7d4cfdd618195c4240b12a097c0c8030e6
lucasfarmwald <farmwaldlucas@gmail.com>
2021-08-16 20:33:26 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+ let result
+ if  (someNumber >= 199){
+   return "This one is on me!";
+ }
+ if  (someNumber >= 2000){
+   result = (someNumber >= 2000 + 30)
+ return "I will gladly take your thirty bucks."
+ 
+ }
+ if (someNumber >= 2500){
+   return "No can do."
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === "NYC" ?  "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous' :
+     return 'Thank you so much.'
+     case 'not as generous' :
+       return 'Thank you.'
+        default: 
+       return 'Bye.'
+  }
 }
